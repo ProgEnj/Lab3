0a0dc80707ccb36a3c7f5f3f63a9dfafdbc28896
Dmitriy <web11rider@gmail.com>
2023-09-23 17:23:55 +0300

    Done until abstract Plant

--- project repos/Farm.cs
+++ project repos/Farm.cs
@@ -6,11 +6,24 @@ public class Farm
     public Farmer? Farmer { get; private set; }
 
     private int _plantsLimit;
-    public string Name { get; set; }
     public int PlantsLimit => _plantsLimit;
-
+    private string name;
+    public string Name
+    {
+        get { return name; }
+        set
+        {
+            if (value.Length > 15)
+            {
+                throw new Exception("Length exceeds 30 characters");
+            }
+            else { name = value; }
+        }
+    }
+    
     public Farm(string name, int limit, Farmer? farmer)
     {
+        Name = name;
         _plantsLimit = limit;
         PlantsOnGrowing = new List<Plant>(20);
         Farmer = farmer;
@@ -24,4 +37,30 @@ public class Farm
     
     public Farm() : this("Farm")
     {}
+    
+    public bool AddPlant(Plant plant)
+    {
+        if (PlantsOnGrowing.Count == PlantsLimit)
+        {
+            return false;
+        }
+        PlantsOnGrowing.Add(plant);
+        return true;
+    }
+    
+    public bool DeletePlant(Plant plant)
+    {
+        return PlantsOnGrowing.Remove(plant);
+    }
+    
+    public Plant? FindByPlantName(string name)
+    {
+        return PlantsOnGrowing.Find(x => name == x.FullName || name == x.ShortName);
+    }
+
+    public void AddFarmer(Farmer? farmer)
+    {
+        Farmer = farmer;
+    }
+
 }
--- project repos/Farmer.cs
+++ project repos/Farmer.cs
@@ -2,8 +2,34 @@
 
 public class Farmer
 {
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
+    private string firstName;
+    public string FirstName
+    {
+        get { return firstName; }
+        set
+        {
+            if (value.Length > 15)
+            {
+                throw new Exception("Length exceeds 30 characters");
+            }
+            else
+            { firstName = value; }
+        }
+    }
+    private string lastName;
+    public string LastName
+    {
+        get { return lastName; }
+        set
+        {
+            if (value.Length > 15)
+            {
+                throw new Exception("Length exceeds 30 characters");
+            }
+            else
+            { lastName = value; }
+        }
+    }
     public string Specialization { get; set; }
 
     public Farmer(string firstName, string lastName, string specialization)
