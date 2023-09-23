be66649131d06cd8c0170e92327ce65cbee12cf1
Dmitriy <web11rider@gmail.com>
2023-09-23 16:08:58 +0300

    Completed first task

--- project repos/Farmer.cs
+++ project repos/Farmer.cs
@@ -5,4 +5,11 @@ public class Farmer
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string Specialization { get; set; }
+
+    public Farmer(string firstName, string lastName, string specialization)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Specialization = specialization;
+    }
 }
--- project repos/Plant.cs
+++ project repos/Plant.cs
@@ -6,4 +6,12 @@ public class Plant
     public string FullName { get; }
     public int GrowingTime { get; }
     public bool IsNeedRegrow { get; }
+
+    private Plant(string shortName, string fullName, int growingTime, bool isNeedRegrow)
+    {
+        ShortName = shortName;
+        FullName = fullName;
+        GrowingTime = growingTime;
+        IsNeedRegrow = isNeedRegrow;
+    }
 }
